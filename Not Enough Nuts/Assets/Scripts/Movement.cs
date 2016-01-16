6cacc49446faca24c9f25e7b11b3be5c3f813d69
jrabaino <jrabaino@uci.edu>
2016-01-15 19:55:43 -0800

    Test

---
+++ Not Enough Nuts/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    
+    public float speedRight;
+    public float speedLeft;
+    public float speedUp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        if (Input.GetKeyDown("space") & transform.position.y <= .2 )
+        {
+
+            
+            transform.position += Vector3.up * speedUp * Time.deltaTime;
+        }
+ 
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speedRight * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speedLeft * Time.deltaTime;
+        }
+ 
+         
+   
+     
+	
+	}
+}
