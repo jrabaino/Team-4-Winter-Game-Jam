7843472e581bd90ee8fa1a9f95ef03292a1cf6d6
Jeney Lao <jeneyl@uci.edu>
2016-01-15 19:44:05 -0800

    prefabs
    Added a Squirrel prefab and stuffz

---
+++ Not Enough Nuts/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private int nutCount = 0;
+    private int nutGoal = 5;
+
+    private Transform bulletSpawn;
+    private Object bulletPrefab;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        bulletSpawn = this.transform.FindChild("BulletSpawn");
+        bulletPrefab = Resources.Load("bullet");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.GetKeyDown(KeyCode.E))
+        {
+            ShootNuts();
+        }
+          
+	}
+
+
+    public void ShootNuts()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = bulletSpawn.position;
+    }
+
+    public int GetNutCount()
+    {
+        return nutCount;
+    }
+
+
+
+    public int GetNutGoal()
+    {
+        return nutGoal;
+    }
+
+
+
+
+
+
+
+}
