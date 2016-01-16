b473979f6e598cb88555e550b725f2cffbfea644
kinseyragequit <kinseylynaugh@gmail.com>
2016-01-15 20:57:26 -0800

    Implemented Timer part of UI

---
+++ Not Enough Nuts/Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+//using Math;
+public class Timer : MonoBehaviour
+{
+    public double startTime = 9.0;
+    public double timeLeft = 0.0;
+    public Text timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        //StartCoroutine (countdown ());
+        timeLeft = startTime;
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        int secondsLeft = (int)timeLeft;
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft % 60;
+        string s = seconds.ToString();
+        if (seconds < 10)
+        {
+            s = "0" + s;
+        }
+
+        timer.text = minutes.ToString() + ":" + s;
+        if (timeLeft <= 0)
+        {
+                SceneManager.LoadScene("GameOver");
+                timeLeft = 0.0;
+        }
+
+
+
+    }
+}
