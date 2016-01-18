b8ff26f3cbc1466755897c1fcbf4e4c5a012d658
Jeney Lao <jeneyl@uci.edu>
2016-01-17 17:06:51 -0800

    Title Screen movement
    Title Screen movement

---
+++ Not Enough Nuts/Assets/Scripts/Follow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    private Rigidbody2D rigidbody_2d;
+    int hit = 1;
+    float x = 100.0f;
+    float y = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        rigidbody_2d = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody_2d.AddForce(new Vector3(x, y, 0.0f));
+	}
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Wall")
+        {
+            Debug.Log("HIT!");
+            x = -x;
+            y = -y;
+        }
+    }
+}
