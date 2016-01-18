a600a99057c5d642065094a4f2be78572aaf7796
jrabaino <jrabaino@uci.edu>
2016-01-17 17:40:54 -0800

    Added Title Song

--- Not Enough Nuts/Assets/Scripts/StartGame.cs
+++ Not Enough Nuts/Assets/Scripts/StartGame.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class StartGame : MonoBehaviour {
-
+    public AudioSource Title_Song;
 	// Use this for initialization
 	void Start () {
-	
+
+        Title_Song.Play();
 	}
     public void onClick()
     {
+        Title_Song.Stop();
         Application.LoadLevel("Level1");
     }
 	// Update is called once per frame
